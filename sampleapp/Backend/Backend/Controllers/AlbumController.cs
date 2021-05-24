b4bfa96367e0a4065569abf23f278aa976b8fe25
CORP\ca185144 <ca185144@ncr.com>
2021-05-24 15:50:39 -0400

    Adding all new files

---
+++ sampleapp/Backend/Backend/Controllers/AlbumController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+
+
+namespace Backend.Controllers
+{
+    
+
+    [Route("api/[controller]")]
+    public class AlbumController : Controller
+    {
+        private const string url = "https://jsonplaceholder.typicode.com/albums";
+        [HttpGet]
+        public async Task<IEnumerable<Album>> GetAlbums()
+        {
+            HttpClient client = new HttpClient(); 
+            HttpResponseMessage response = await client.GetAsync(new Uri(url));
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsAsync<IEnumerable<Album>>();
+
+            }
+            return null;
+        }
+    }
+}
