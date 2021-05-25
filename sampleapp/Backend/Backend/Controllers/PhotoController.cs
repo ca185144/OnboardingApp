cd1dfce7f15c719adfeab6397a29871ff14d75e7
CORP\ca185144 <ca185144@ncr.com>
2021-05-25 11:02:51 -0400

    Fully functional Sample App

---
+++ sampleapp/Backend/Backend/Controllers/PhotoController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+
+
+namespace Backend.Controllers
+{
+    
+
+    [Route("api/[controller]")]
+    public class PhotoController : Controller
+    {
+        private const string url = "https://jsonplaceholder.typicode.com/photos";
+        [HttpGet]
+        public async Task<IEnumerable<Photo>> GetPhotos()
+        {
+            HttpClient client = new HttpClient(); 
+            HttpResponseMessage response = await client.GetAsync(new Uri(url));
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsAsync<IEnumerable<Photo>>();
+
+            }
+            return null;
+        }
+        
+        [HttpGet("{albumId:long}/all")]
+        public async Task<IEnumerable<Photo>> GetPhotosByAlbum(long albumId)
+        {
+            HttpClient client = new HttpClient(); 
+            HttpResponseMessage response = await client.GetAsync(new Uri(url + "?albumId=" + albumId));
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsAsync<IEnumerable<Photo>>();
+
+            }
+            return null;
+        }
+        
+        [HttpGet("{albumId:long}")]
+        public async Task<IEnumerable<Photo>> GetFirstPhotoByAlbum(long albumId)
+        {
+            HttpClient client = new HttpClient(); 
+            HttpResponseMessage response = await client.GetAsync(new Uri(url + "?albumId=" + albumId + "&_limit=1"));
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsAsync<IEnumerable<Photo>>();
+
+            }
+            return null;
+        }
+        
+    }
+}
---
+++ sampleapp/Backend/Backend/Models/Photo.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.Models
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        public int AlbumId { get; set; }
+        public string ThumbnailUrl { get; set; }
+    }
+}
